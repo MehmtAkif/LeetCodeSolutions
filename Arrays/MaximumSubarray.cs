a116e3b566cab4368d5830b91bcc05f7545cc864
MehmtAkif <akifdemirci55150@gmail.com>
2026-03-11 15:14:40 +0300

    feat: Maximum Subarray #53 çözümü eklendi

---
+++ Arrays/MaximumSubarray.cs
@@ -0,0 +1,29 @@
+﻿// ==============================
+// #53 - Maximum Subarray
+// Zorluk: Medium
+// Konu: Array, Dynamic Programming
+// Tarih: 11.03.2026
+// ==============================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0];
+            int currentSum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+            return maxSum;
+        }
+    }
+}
--- LeetCode.Runner/Program.cs
+++ LeetCode.Runner/Program.cs
@@ -1,10 +1,6 @@
 ﻿// Test 1
 using Arrays;
 
-int[] nums4 = { 1, 2, 3, 1 };
-ContainsDuplicate duplicate = new ContainsDuplicate();
-Console.WriteLine($"Test 1: {duplicate.ContainsDuplicateMethod(nums4)}"); // Beklenen: True
-
-// Test 2
-int[] nums5 = { 1, 2, 3, 4 };
-Console.WriteLine($"Test 2: {duplicate.ContainsDuplicateMethod(nums5)}"); // Beklenen: False
+int[] nums3 = { 5, 4, -1, 7, 8 };
+MaximumSubarray maximumSubarray = new MaximumSubarray();
+Console.WriteLine($"Test 1: {maximumSubarray.MaxSubArray(nums3)}");
