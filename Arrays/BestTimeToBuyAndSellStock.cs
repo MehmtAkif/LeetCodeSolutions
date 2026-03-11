45f15b44f2b2c07a050fb6be6ee6b0bbe1972a5d
MehmtAkif <akifdemirci55150@gmail.com>
2026-03-11 12:54:31 +0300

    feat: Best Time to Buy and Sell Stock #121 çözümü eklendi

---
+++ Arrays/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,32 @@
+﻿// ==============================
+// #121 - Best Time to Buy and Sell Stock
+// Zorluk: Easy
+// Konu: Array
+// Tarih: 11.03.2026
+// ==============================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach(int price in prices)
+            {
+                if (price < minPrice)
+                    minPrice = price;
+                else if (price - minPrice > maxProfit)
+                    maxProfit = price - minPrice;
+            }
+            return maxProfit;
+        }
+    }
+}
--- LeetCode.Runner/Program.cs
+++ LeetCode.Runner/Program.cs
@@ -1,20 +1,11 @@
 ﻿using Arrays;
 
+
 // Test 1
-int[] nums = { 2, 7, 11, 15 };
-int target = 9;
-TwoSum twoSum = new TwoSum();
-int[] result = twoSum.Solve(nums, target);
-Console.WriteLine($"Test 1 Output: [{result[0]}, {result[1]}]");
+int[] prices1 = { 7, 1, 5, 3, 6, 4 };
+BestTimeToBuyAndSellStock stock = new BestTimeToBuyAndSellStock();
+Console.WriteLine($"Test 1: {stock.MaxProfit(prices1)}"); // Beklenen: 5
 
 // Test 2
-int[] nums2 = { 3, 4, 1, 1, 2 };
-int target2 = 6;
-int[] result2 = twoSum.Solve(nums2, target2);
-Console.WriteLine($"Test 2 Output: [{result2[0]}, {result2[1]}]");
-
-// Test 3
-int[] nums3 = { 3, 3 };
-int target3 = 6;
-int[] result3 = twoSum.Solve(nums3, target3);
-Console.WriteLine($"Test 3 Output: [{result3[0]}, {result3[1]}]");
+int[] prices2 = { 7, 6, 4, 3, 1 };
+Console.WriteLine($"Test 2: {stock.MaxProfit(prices2)}"); // Beklenen: 0
